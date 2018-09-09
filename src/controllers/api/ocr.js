20d98ab2ca740b356dd73ae7580a0113e6805296
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-09-10 00:31:20 +0800

    Add OCR method for recognizing images with tesseract

--- src/controllers/api/ocr.js
+++ src/controllers/api/ocr.js
@@ -1,3 +1,28 @@
-export const ocr = () => {
-  console.log('In OCR api method!');
+import isEmpty from 'lodash/fp/isEmpty';
+import path from 'path';
+import rp from 'request-promise-native';
+import tesseractjs from 'tesseract.js';
+
+const Tesseract = tesseractjs.create({
+  workerPath: path.join(__dirname, '../../lib/tesseract/src/node/worker.js'),
+  langPath:   path.join(__dirname, '../../lib/tesseract/lang/'),
+  corePath:   path.join(__dirname, '../../lib/tesseract/src/core.js')
+});
+
+export const ocr = (req, res) => {
+  if (!isEmpty(req.query.image_url)) {
+    const imageURL = req.query.image_url;
+
+    rp(imageURL, { encoding: null })
+    .then(data => {
+      return Tesseract.recognize(data);
+    })
+    .then((result) => {
+      console.log(`[Tesseract Recognize Finish] result.text: ${result.text}`);
+      Tesseract.terminate();
+    })
+    .catch(error => {
+      console.log(`ERROR requesting ${imageURL}: ${JSON.stringify(error)}`);
+    });
+  }
 };
