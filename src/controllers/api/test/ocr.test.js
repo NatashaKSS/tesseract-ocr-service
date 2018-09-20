b6e19354e89b4e306d6b961558340d66d4dc4022
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-09-20 22:43:01 +0800

    Remove files used for running a nodejs server

--- src/controllers/api/test/ocr.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import someFunc from '../someFunc';
-
-describe('[someFunc.js] Test cases', () => {
-  it('Should return \'from the other side!\'', () => {
-    expect(someFunc()).to.equal('from the other side!');
-  });
-});
--- src/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Entry point for this web app.
- */
-import startServer from './server/server';
-
-startServer();
